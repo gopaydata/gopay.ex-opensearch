f9576b4d73244a70fb5626c059ee1b80b2fd3243
gopay-keboola <109345035+gopay-keboola@users.noreply.github.com>
2025-01-21 11:26:22 +0100

    Update component.py

--- src/component.py
+++ src/component.py
@@ -1,25 +1,24 @@
-import json
+# import json
+import datetime
 import logging
-import uuid
-import os
-import shutil
-import dateparser
-import pytz
+import csv
+# import time
+import pandas as pd
 
 import requests
 from requests.auth import HTTPBasicAuth
 
-import csv
-import datetime
-
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
-from keboola.csvwriter import ElasticDictWriter
 
-from client.es_client import ElasticsearchClient
-from legacy_client.legacy_es_client import LegacyClient
-from client.ssh_utils import SomeSSHException, get_private_key
+# from client.es_client import ElasticsearchClient
+from client.ssh_utils import get_private_key
 from sshtunnel import SSHTunnelForwarder
+
+# from requests.auth import AuthBase
+# from requests.models import Response
+
+# import traceback
 
 # configuration variables
 KEY_GROUP_DB = 'db'
@@ -71,63 +70,21 @@ class Component(ComponentBase):
     def __init__(self):
         super().__init__()
 
-    def run(self):
-        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        params = self.configuration.parameters
-
-        if params.get(KEY_LEGACY_SSH):
-            self.run_legacy_client()
-        else:
-            out_table_name = params.get(KEY_STORAGE_TABLE, False)
-            if not out_table_name:
-                out_table_name = "ex-elasticsearch-result"
-                logging.info(f"Using default output table name: {out_table_name}")
-
-            user_defined_pk = params.get(KEY_PRIMARY_KEYS, [])
-            incremental = params.get(KEY_INCREMENTAL, False)
-
-            index_name, query = self.parse_index_parameters(params)
-            statefile = self.get_state_file()
-
-            ssh_options = params.get(KEY_SSH)
-            # fix eternal KBC issue
-            if not isinstance(ssh_options, list):
-                if ssh_options.get(KEY_USE_SSH, False):
-                    self._create_and_start_ssh_tunnel(params)
-
-            client = self.get_client(params)
-
-            temp_folder = os.path.join(self.data_folder_path, "temp")
-            os.makedirs(temp_folder, exist_ok=True)
-
-            columns = statefile.get(out_table_name, [])
-            out_table = self.create_out_table_definition(out_table_name, primary_key=user_defined_pk,
-                                                         incremental=incremental)
-
-            try:
-                with ElasticDictWriter(out_table.full_path, columns) as wr:
-                    for result in client.extract_data(index_name, query):
-                        wr.writerow(result)
-                    wr.writeheader()
-            except Exception as e:
-                raise UserException(f"Error occured while extracting data from Elasticsearch: {e}")
-            finally:
-                if hasattr(self, 'ssh_tunnel') and self.ssh_tunnel.is_active:
-                    self.ssh_tunnel.stop()
-
-            self.write_manifest(out_table)
-            statefile[out_table_name] = wr.fieldnames
-            self.write_state_file(statefile)
-            self.cleanup(temp_folder)
-
     @staticmethod
-    def run_legacy_client() -> None:
-        client = LegacyClient()
-        client.run()
-
-    @staticmethod
-    def cleanup(temp_folder: str):
-        shutil.rmtree(temp_folder)
+    def validate_params(params):
+        """Validates the configuration parameters."""
+        logging.info("Validating configuration parameters...")
+        if KEY_GROUP_AUTH not in params:
+            raise UserException(f"Missing {KEY_GROUP_AUTH} in parameters.")
+        auth_params = params[KEY_GROUP_AUTH]
+        if not auth_params.get(KEY_API_KEY_ID) or not auth_params.get(KEY_API_KEY):
+            raise UserException("Missing API Key ID or API Key in authentication parameters.")
+        if KEY_GROUP_DB not in params:
+            raise UserException(f"Missing {KEY_GROUP_DB} in parameters.")
+        db_params = params[KEY_GROUP_DB]
+        if not db_params.get(KEY_DB_HOSTNAME) or not db_params.get(KEY_DB_PORT):
+            raise UserException("Missing database hostname or port.")
+        logging.info("Validation successful.")
 
     def test_health(self, params):
         logging.info("OS health testing...")
@@ -206,113 +163,86 @@ class Component(ComponentBase):
         finally:
             logging.info("Po pokusu o uložení souboru.")
 
-    def get_client(self, params: dict) -> ElasticsearchClient:
-        auth_params = params.get(KEY_GROUP_AUTH)
-        if not auth_params:
-            return self.get_client_legacy(params)
+    def query_data(self, params):
+        logging.info("OS health testing...")
 
-        db_params = params.get(KEY_GROUP_DB)
-        db_hostname = db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
-        scheme = params.get(KEY_SCHEME, "http")
+        local_host = 'os.gopay.com'
+        local_port = '443'
 
-        auth_type = auth_params.get(KEY_AUTH_TYPE, False)
-        if auth_type not in ["basic", "api_key", "bearer", "no_auth"]:
-            raise UserException(f"Invalid auth_type: {auth_type}")
-
-        setup = {"host": db_hostname, "port": db_port, "scheme": scheme}
-
-        logging.info(f"The component will use {auth_type} type authorization.")
-
-        if auth_type == "basic":
-            username = auth_params.get(KEY_USERNAME)
-            password = auth_params.get(KEY_PASSWORD)
-
-            if not (username and password):
-                raise UserException("You must specify both username and password for basic type authorization")
-
-            auth = (username, password)
-            client = ElasticsearchClient([setup], scheme, http_auth=auth)
-
-        elif auth_type == "api_key":
-            api_key_id = auth_params.get(KEY_API_KEY_ID)
-            api_key = auth_params.get(KEY_API_KEY)
-            api_key = (api_key_id, api_key)
-            client = ElasticsearchClient([setup], scheme, api_key=api_key)
-
-        elif auth_type == "no_auth":
-            client = ElasticsearchClient([setup], scheme)
-
+        # Kontrola stavu SSH tunelu
+        if hasattr(self, "ssh_tunnel") and self.ssh_tunnel.is_active:
+            logging.info("OK - Tunnel is active.")
+            logging.info(self.ssh_tunnel.is_active)
+            local_host, local_port = self.ssh_tunnel.local_bind_address
         else:
-            raise UserException(f"Unsupported auth_type: {auth_type}")
+            logging.warning("SSH tunnel is not active or not configured.")
 
-        try:
-            p = client.ping(error_trace=True)
-            if not p:
-                raise UserException(f"Connection to Elasticsearch instance {db_hostname}:{db_port} failed.")
-        except Exception as e:
-            raise UserException(f"Connection to Elasticsearch instance {db_hostname}:{db_port} failed. {str(e)}")
+            # Sestavení URL
+            url = f"https://{local_host}:{local_port}/app-logs-prod/_search"
 
-        return client
+            auth_params = params.get(KEY_GROUP_AUTH, {})
+            username = auth_params.get(KEY_API_KEY_ID)
+            password = auth_params.get(KEY_API_KEY)
 
-    @staticmethod
-    def get_client_legacy(params) -> ElasticsearchClient:
-        db_params = params.get(KEY_GROUP_DB)
-        db_hostname = db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
+            logging.info("Connecting to " + url)
+            logging.info("Username: " + username)
 
-        setup = {"host": db_hostname, "port": db_port, "scheme": "http"}
-        client = ElasticsearchClient([setup])
+            # Parametry pro připojení k OpenSearch
+            # url = "https://os.gopay.com/app-logs-prod/_search"
+            # username = "keboola"  # Zadej uživatelské jméno
+            # password = "TFzak4.iFE_mFvz4Caw"  # Zadej heslo
 
-        return client
 
-    def parse_index_parameters(self, params):
-        index = params.get(KEY_INDEX_NAME, "")
-        date_config = params.get(KEY_GROUP_DATE, {})
-        query = self._parse_query(params)
+            query = {
+                "query": {
+                    "query_string": {
+                        "query": "8997989023"  # Číslo platby
+                    }
+                },
+                "size": 10000  # Zvýšení limitu počtu výsledků
+            }
 
-        if DATE_PLACEHOLDER in index:
-            index = self._replace_date_placeholder(index, date_config)
+            # Odeslání požadavku s Basic Auth
+            response = requests.post(url, auth=HTTPBasicAuth(username, password), json=query)
 
-        return index, query
+            if response.status_code == 200:
+                # Parsování JSON odpovědi
+                data = response.json()
+                hits = data.get("hits", {}).get("hits", [])
 
-    @staticmethod
-    def _parse_query(params):
-        _query = params.get(KEY_QUERY, '{}').strip()
-        query_string = _query if _query != '' else '{}'
+                if hits:
+                    # Převod záznamů do DataFrame
+                    df = pd.DataFrame(hits)
 
-        try:
-            logging.info(f"Using query: {query_string}")
-            return json.loads(query_string)
-        except ValueError:
-            raise UserException("Could not parse request body string to JSON.")
+                    # Bezpečné načtení JSON dat z '_source'
+                    def parse_json(source):
+                        try:
+                            return json.loads(source) if isinstance(source, str) else source
+                        except json.JSONDecodeError:
+                            return {}
 
-    def _replace_date_placeholder(self, index, date_config):
-        _date = dateparser.parse(date_config.get(KEY_DATE_SHIFT, DEFAULT_DATE))
-        if _date is None:
-            raise UserException(f"Could not parse value {date_config[KEY_DATE_SHIFT]} to date.")
+                    # Rozbalení JSON sloupce '_source'
+                    df['_source'] = df['_source'].apply(parse_json)
+                    source_expanded = pd.json_normalize(df['_source'])
 
-        _date = _date.replace(tzinfo=pytz.UTC)
-        _tz = self._validate_timezone(date_config.get(KEY_DATE_TZ, DEFAULT_TZ))
-        _date_tz = pytz.timezone(_tz).normalize(_date)
-        _date_formatted = _date_tz.strftime(date_config.get(KEY_DATE_FORMAT, DEFAULT_DATE_FORMAT))
+                    # Spojení s původními sloupci (_id a _index)
+                    expanded_data = pd.concat([df[['_id', '_index']], source_expanded], axis=1)
 
-        logging.info(f"Replaced date placeholder with value {_date_formatted}. "
-                     f"Downloading data from index {index.replace(DATE_PLACEHOLDER, _date_formatted)}.")
-        return index.replace(DATE_PLACEHOLDER, _date_formatted)
+                    # Cesta k výstupnímu CSV souboru
+                    csv_file = self.create_out_table_definition("payment_logs.csv")
+                    out_table_path = csv_file.full_path
 
-    @staticmethod
-    def _validate_timezone(tz):
-        if tz not in pytz.all_timezones:
-            raise UserException(f"Incorrect timezone {tz} provided. Timezone must be a valid DB timezone name. "
-                                "See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List.")
-        return tz
+                    # Uložení výsledků do CSV
+                    expanded_data.to_csv(out_table_path, index=False)
+                    print(f"Data byla úspěšně uložena do souboru: {out_table_path}")
+                else:
+                    print("Nebyla nalezena žádná data odpovídající dotazu.")
+            else:
+                print(f"Chyba při odesílání požadavku: {response.status_code}")
+                print(response.text)
 
-    @staticmethod
-    def _save_results(results: list, destination: str) -> None:
-        full_path = os.path.join(destination, f"{uuid.uuid4()}.json")
-        with open(full_path, "w") as json_file:
-            json.dump(results, json_file, indent=4)
+            response = requests.get(url, auth=HTTPBasicAuth(username, password), timeout=100, verify=False)
+            logging.info("Response code:" + str(response.status_code))
 
     def _create_and_start_ssh_tunnel(self, params):
         """Sets up and starts the SSH tunnel."""
@@ -348,58 +278,55 @@ class Component(ComponentBase):
             logging.error(f"Failed to start SSH tunnel: {e}")
             raise UserException(f"Failed to establish SSH tunnel: {e}")
 
-        # Test
-        try:
-            self.test_health(params)
-        except Exception as e:
-            logging.error(f"Test selhal: {e}")
-        finally:
-            logging.info("Konec testu.")
-
     @staticmethod
-    def is_valid_rsa(rsa_key) -> (bool, str):
+    def is_valid_rsa(rsa_key):
+        """Validates the RSA private key."""
         if not rsa_key.startswith(RSA_HEADER):
-            return False, f"The RSA key does not start with the correct header: {RSA_HEADER}"
+            return False, "RSA key does not start with the correct header."
         if "\n" not in rsa_key:
-            return False, "The RSA key does not contain any newline characters."
+            return False, "RSA key does not contain newline characters."
         return True, ""
 
-    def _create_ssh_tunnel(self, ssh_username, private_key, ssh_tunnel_host, ssh_tunnel_port,
-                           db_hostname, db_port) -> None:
+    def run(self):
+        """Main execution logic for the component."""
+        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
+        params = self.configuration.parameters
+        self.validate_params(params)
 
-        is_valid, error_message = self.is_valid_rsa(private_key)
-        if is_valid:
-            logging.info("SSH tunnel is enabled.")
-        else:
-            raise UserException(f"Invalid RSA key provided: {error_message}")
-
+        ssh_tunnel_started = False
         try:
-            private_key = get_private_key(private_key, None)
-        except SomeSSHException as e:
-            raise UserException(e) from e
+            logging.info("Starting component execution...")
 
-        try:
-            db_port = int(db_port)
-        except ValueError as e:
-            raise UserException("Remote port must be a valid integer") from e
+            # Set up SSH tunnel if enabled
+            if params.get(KEY_SSH, {}).get(KEY_USE_SSH, False):
+                logging.info("SSH tunneling is enabled. Setting up...")
+                self._create_and_start_ssh_tunnel(params)
+                ssh_tunnel_started = True
 
-        self.ssh_server = SSHTunnelForwarder(ssh_address_or_host=ssh_tunnel_host,
-                                             ssh_port=ssh_tunnel_port,
-                                             ssh_pkey=private_key,
-                                             ssh_username=ssh_username,
-                                             remote_bind_address=(db_hostname, db_port),
-                                             local_bind_address=(LOCAL_BIND_ADDRESS, db_port),
-                                             ssh_config_file=None,
-                                             allow_agent=False)
+            # Test
+            try:
+                self.test_health(params)
+                self.query_data(params)
+            except Exception as e:
+                logging.error(f"Test selhal: {e}")
+            finally:
+                logging.info("Konec testu.")
+
+        except Exception as e:
+            logging.error(f"Unexpected error during component execution: {type(e).__name__} - {str(e)}")
+            raise
+        finally:
+            if ssh_tunnel_started and hasattr(self, "ssh_tunnel") and self.ssh_tunnel.is_active:
+                logging.info("Stopping SSH tunnel...")
+                self.ssh_tunnel.stop()
+                logging.info("SSH tunnel stopped.")
 
 
-"""
-        Main entrypoint
-"""
+# Main entrypoint
 if __name__ == "__main__":
     try:
+        logging.basicConfig(level=logging.INFO)
         comp = Component()
-        # this triggers the run method by default and is controlled by the configuration.action parameter
         comp.execute_action()
     except UserException as exc:
         logging.exception(exc)
